bb8b2d35067ed1b9c029ae0c711e8633589fcb1a
Serhii0080 <startap.chrom@gmail.com>
2026-06-10 22:27:33 +0300

    add task HW4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,10 +18,9 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
+        + (disabled ? ' ' + s.disabled
+            : xType === 'secondary' ? ' ' + s.secondary
+                : xType === 'red' ? ' ' + s.red : ' ' + s.default)
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
